3bcfd3d1a1c58719eed26439e4850cb70ee6f808
Christoph <max@muster.mann>
2016-02-06 22:40:33 +0100

    updated setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ldaforyelpchallenge',
+      version='1.0',
+      description='Run Latent Dirichlet Allocation on the reviews provided by Yelp for the Yelp data challenge',
+      url='https://github.com/koschr/ldaforyelpchallenge',
+      author='CK',
+      author_email='test@example.com',
+      license='MIT',
+      packages=['ldaforyelpchallenge'],
+      zip_safe=False)
