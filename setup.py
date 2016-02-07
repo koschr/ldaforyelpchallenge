a9b1e2403c83b8435ae5407c5e1bcf2ab4eeb2e5
unknown <i@example.com>
2016-02-07 19:02:56 +0100

    retry

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='ldaforyelpchallenge',
-      version='1.0',
+setup(name='ldayelpchallenge',
+      version='0.1',
       description='Run Latent Dirichlet Allocation on the reviews provided by Yelp for the Yelp data challenge',
       url='https://github.com/koschr/ldaforyelpchallenge',
       download_url='https://github.com/koschr/ldaforyelpchallenge/tarball/1.0',
       author='CK',
       author_email='test@example.com',
       license='MIT',
-      packages=['ldaforyelpchallenge'],
+      packages=['ldayelpchallenge'],
       install_requires=['gensim', 'numpy', 'nltk', 'stop_words', 'langdetect'],
       zip_safe=False)
